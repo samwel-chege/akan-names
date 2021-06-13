29a0b0b46475bba4eb7a7d59ae98bc62a0592198
samwel-chege <samwelchegeh09@gmail.com>
2021-06-13 18:04:50 +0300

    input functions

--- static/JS/main.js
+++ static/JS/main.js
@@ -1,24 +1,18 @@
-function generate() {
-    var day = document.getElementById("day").nodeValue;
-    var dd = parseInt(day);
-    var month = document.getElementById("month").nodeValue;
-    var mm = parseInt(month);
-    var year = document.getElementById("year").nodeValue;
-    var yy = parseInt(year.toString().slice(2, 4));
-    var cc = Math.ceil(year/100);
-    var result = parseInt( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
-    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    if (document.getElementById("gender").checked) {
-      var gender = "male";
-    }
-    else{
-        var gender = "female";
-    }
-    if (dd <=0 || dd > 31) {
+var form = document.getElementById('form')
+form.addEventListener("submit", function generate(event){
+    event.preventDefault()
+    let dd = document.getElementById("day").value;
+    let mm = document.getElementById("month").value;
+    let yy = document.getElementById("year").value;
+    let cc = document.getElementById("century").value;
+    let gender = document.getElementById("gender").value;
+    let result = parseInt( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
+    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    if (dd <= 0 || dd > 31) {
         alert("invalid date");
     }
-    else if (mm <= 0 || mm > 31) {
+    else if (mm <= 0 || mm > 12) {
         alert("invalid month");
     }
     else if (Math.round(result) == 0 && gender === "male"){
@@ -69,3 +63,4 @@ function generate() {
     }
 
 }
+)
